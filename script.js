e2f2cd0b956b138865b96e0d3118f79e74858c96
saydamsadi <50597798+saydamsadi@users.noreply.github.com>
2023-03-17 22:51:56 +0100

    responsive eclipse with turkish and american separated

--- script.js
+++ script.js
@@ -4,18 +4,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const iconsSrc = [
-    'https://www.logo.wine/a/logo/Tesla%2C_Inc./Tesla%2C_Inc.-Logo.wine.svg',
-    'https://www.logo.wine/a/logo/Apple_Inc./Apple_Inc.-Logo.wine.svg',
-    'https://www.logo.wine/a/logo/Microsoft/Microsoft-Logo.wine.svg',
-    'https://www.logo.wine/a/logo/Amazon_(company)/Amazon_(company)-Logo.wine.svg',
-    'https://www.logo.wine/a/logo/Meta_Platforms/Meta_Platforms-Logo.wine.svg',
-];
-
 const screenSizeFactor = Math.min(canvas.width, canvas.height) / 1080;
 const iconSize = 100 * screenSizeFactor;
-const numIcons = 25;
-
 const speedFactor = screenSizeFactor;
 
 const centerX = canvas.width / 2;
@@ -23,21 +13,34 @@ const centerY = canvas.height / 2;
 
 const ellipseParams = [
     {
-        width: 1105.13 * screenSizeFactor,
-        height: 507.96 * screenSizeFactor,
+        width: Math.min(canvas.width * 0.9, 1105.13 * screenSizeFactor),
+        height: Math.min(canvas.height * 0.9, 507.96 * screenSizeFactor),
         rotation: -2.35 * (Math.PI / 180)
     },
     {
-        width: 1090 * screenSizeFactor,
-        height: 641 * screenSizeFactor,
+        width: Math.min(canvas.width * 0.9, 1090 * screenSizeFactor),
+        height: Math.min(canvas.height * 0.9, 641 * screenSizeFactor),
         rotation: 15.45 * (Math.PI / 180)
     }
 ];
 
+const iconsSrc = [
+    [
+        'https://static.wixstatic.com/media/7f220a_0509f35f325b46e98bad1a96a603399b~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_c8ef01789e914a97a3c05b449afa7d0c~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_8b64d76032f04718ae00b1715aaaa2ef~mv2.png',
+    ],
+    [
+        'https://static.wixstatic.com/media/7f220a_b0b0cd00f4e243a589dc6fe0d51d0fef~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_56412b2e91ef44cc82d94473316c339a~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_3887544aea9e47f2992b599f04600b55~mv2.png',
+    ]
+];
+
 class StockIcon {
-    constructor(angle, ellipseIndex) {
+    constructor(angle, ellipseIndex, iconIndex) {
         this.image = new Image();
-        this.image.src = iconsSrc[Math.floor(Math.random() * iconsSrc.length)];
+        this.image.src = iconsSrc[ellipseIndex][iconIndex];
         this.size = iconSize;
         this.angle = angle;
         this.speed = (Math.random() * 2 - 1) * 0.02 * speedFactor;
@@ -60,10 +63,11 @@ class StockIcon {
 }
 
 const stockIcons = [];
-for (let i = 0; i < numIcons; i++) {
-    const angle = (i / numIcons) * (2 * Math.PI);
-    const ellipseIndex = i % 2;
-    stockIcons.push(new StockIcon(angle, ellipseIndex));
+for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 3; j++) {
+        const angle = (j / 3) * (2 * Math.PI);
+        stockIcons.push(new StockIcon(angle, i, j));
+    }
 }
 
 function drawEllipse(ellipse) {
@@ -103,5 +107,13 @@ animate();
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-});
 
+    ellipseParams[0].width = Math.min(canvas.width * 0.9, 1105.13 * screenSizeFactor);
+    ellipseParams[0].height = Math.min(canvas.height * 0.9, 507.96 * screenSizeFactor);
+
+    ellipseParams[1].width = Math.min(canvas.width * 0.9, 1090 * screenSizeFactor);
+    ellipseParams[1].height = Math.min(canvas.height * 0.9, 641 * screenSizeFactor);
+
+    centerX = canvas.width / 2;
+    centerY = canvas.height / 2;
+});
