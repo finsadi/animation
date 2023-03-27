20de303dd76318d13463bb4cc9029de4f43b328f
saydamsadi <50597798+saydamsadi@users.noreply.github.com>
2023-03-28 00:29:26 +0200

    JS updated for removed bg and added icons

--- script.js
+++ script.js
@@ -26,16 +26,21 @@ const ellipseParams = [
 
 const iconsSrc = [
     [
-        'https://static.wixstatic.com/media/7f220a_0509f35f325b46e98bad1a96a603399b~mv2.png',
-        'https://static.wixstatic.com/media/7f220a_c8ef01789e914a97a3c05b449afa7d0c~mv2.png',
-        'https://static.wixstatic.com/media/7f220a_8b64d76032f04718ae00b1715aaaa2ef~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_3ec1f70b36734641953984743ccb710d~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_3d4d84a1636745f096385b4f284c9e3d~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_317d91e479ba485bb6e1e50e7996098b~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_8afc0acaae9249b48ac4e27b36d194ba~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_9971ac5495bf41918f210f570419a6fd~mv2.png',
     ],
     [
-        'https://static.wixstatic.com/media/7f220a_b0b0cd00f4e243a589dc6fe0d51d0fef~mv2.png',
-        'https://static.wixstatic.com/media/7f220a_56412b2e91ef44cc82d94473316c339a~mv2.png',
-        'https://static.wixstatic.com/media/7f220a_3887544aea9e47f2992b599f04600b55~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_2962da97a26246298035eebd212de50d~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_537b78cb090346d8b12a894acdb8c11e~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_77f9b530122e45ae8cd46e18c22964c2~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_340411b7fcba4c699a41de16366d934c~mv2.png',
+        'https://static.wixstatic.com/media/7f220a_e423f4c9303b4c7087c6e1e84d64312d~mv2.png',
     ]
 ];
+
 
 class StockIcon {
     constructor(angle, ellipseIndex, iconIndex) {
@@ -64,11 +69,12 @@ class StockIcon {
 
 const stockIcons = [];
 for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 3; j++) {
-        const angle = (j / 3) * (2 * Math.PI);
+    for (let j = 0; j < 5; j++) {
+        const angle = (j / 5) * (2 * Math.PI);
         stockIcons.push(new StockIcon(angle, i, j));
     }
 }
+
 
 function drawEllipse(ellipse) {
     ctx.save();
@@ -81,16 +87,7 @@ function drawEllipse(ellipse) {
 }
 
 function animate() {
-    const gradient = ctx.createRadialGradient(
-        centerX, centerY, 0,
-        centerX, centerY, Math.max(canvas.width, canvas.height)
-    );
-
-    gradient.addColorStop(0, '#00E091');
-    gradient.addColorStop(1, 'black');
-
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ellipseParams.forEach(ellipse => drawEllipse(ellipse));
 
@@ -101,6 +98,7 @@ function animate() {
 
     requestAnimationFrame(animate);
 }
+
 
 animate();
 
